1a03ab3c52da6d15440b07cadbf6431ee6f9ae31
Nidhish Sonavale <sonavalenidhish14@gmail.com>
2025-02-11 11:38:34 +0530

    Data Transformation

---
+++ src/components/data_transformation.py
@@ -0,0 +1,20 @@
+import sys
+from dataclasses import dataclass
+import os
+
+import numpy as np
+import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler,OneHotEncoder
+
+from src.exception import CustomException
+from src.logger import logging
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts','preprocessor.pkl')
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
